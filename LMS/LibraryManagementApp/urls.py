7998a9f0f39f17f8d89fcb14abc4a254868752f6
Timotips <kingbam17@gmail.com>
2024-09-12 20:17:28 +0800

    Changes on some page
    -creating a registration page
    -linking the page to others

--- LMS/LibraryManagementApp/urls.py
+++ LMS/LibraryManagementApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Login, name="login"),
+    path('registration/', views.Registration, name="registration"),
     path('dashboard/', views.Dashboard, name="dashboard"),
     path('book-collection/', views.BookCollection, name="book-collection"),
     path('borrow-return/', views.BorrowReturn, name="borrow-return"),
